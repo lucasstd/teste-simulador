76ca42752076f2cfb4718d558a2132cc3b03fc8b
lucasstd <lucas.std1@gmail.com>
2020-03-08 20:54:11 -0300

    elements inside investment simulation added

---
+++ cypress/support/elements/SimulationElements.js
@@ -0,0 +1,10 @@
+elements = {
+    'botaoParaVoce': 'input[value="paraVoce"]',
+    'botaoParaEmpresa': 'input[value="paraEmpresa"]',
+    'inputQuantiaParaAplicar': '#valorAplicar',
+    'inputQuantiaPoupada': '#valorInvestir',
+    'quantoTempo': '#tempo',
+    "tipoTempo": "/html/body/div[3]/div/div/div[1]/div/div[1]/form/div[4]/div[2]/div[2]/a/span[3]"
+}
+
+export default elements;
