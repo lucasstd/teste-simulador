d8acd8720bd355959bdfd02aea99ccee894d6c73
80699237 <lucas.staudt@cwi.com.br>
2020-03-10 12:06:11 -0300

    refactor simulation form and add more test cases

--- cypress/integration/simulationForm.spec.js
+++ cypress/integration/simulationForm.spec.js
@@ -3,62 +3,82 @@ import page from '../support/pageobjects/SimulationPage'
 
 const simulationPage = new page();
 
+// O associado preencher todos os campos corretamente e visualizar o formulário de simulação
 describe('Preencher corretamente a pagina de simulação', () => {
     context('como um associado', () => {
+        before(() => { simulationPage.acessarSite() });
 
-        before(() => {
-            simulationPage.acessarSite()
-        });
-
-        // // O associado preencher todos os campos corretamente e visualizar o
-        // // formulário de simulação
         it('Preenche valor para aplicar com 20,00', () => {
-            simulationPage.inputQuantiaParaAplicar().type('20,00').then(
-                simulationPage.inputQuantiaPoupada().click().then(
-                    cy.get('#valorAplicar-error').should('not.be.visible')
-                )
-            );
-            simulationPage.inputQuantiaPoupada().click();
+            simulationPage.inputQuantiaParaAplicar().type('20,00')
+            simulationPage.inputQuantiaPoupada().click()
+            simulationPage.errorValorAplicarMsg().should('not.be.visible')
         });
-        // O associado preencher o valor inferior a “R$ 20.00” e receber a
-        // mensagem de orientação “Valor mínimo de R$ 20.00"
         it('Preenche valor que você quer poupar com 20,00', () => {
-            simulationPage.inputQuantiaPoupada().type('20,00').then(
-                simulationPage.inputQuantiaTempo().click().then(
-                    cy.get('#valorInvestir-error')
-                        .should('not.be.visible')
-                )
-            );
+            simulationPage.inputQuantiaPoupada().type('20,00')
+            simulationPage.inputQuantiaTempo().click()
+            simulationPage.errorValorInvestirMsg().should('not.be.visible')
         })
-        
-        it('Preenche com 20,00 campo Por quanto tempo você quer poupar', () => {
-            simulationPage.inputQuantiaTempo().type('20').then(
-                simulationPage.btnSimular().click().then(
-                    // cy.get('#valorAplicar-error').should('not.be.visible')
-                )
-            );
+
+        it('Preenche com 2 campo Por quanto tempo você quer poupar', () => {
+            simulationPage.inputQuantiaTempo().type('2')
+            simulationPage.inputQuantiaPoupada().click()
+            simulationPage.errorTempoMsg().should('not.be.visible')
         });
         it('Clicar em Simular', () => {
-            simulationPage.btnSimular().click().then(
-                simulationPage.tabelaComDados().should('not.be.visible')
-            )
+            simulationPage.btnSimular().click()
+            simulationPage.tabelaComDados().should('be.visible')
         });
 
-        after(() => {
-            simulationPage.refazerSimulacao().click
-        })
+        after(() => { simulationPage.refazerSimulacao().click() })
     })
 });
 
-// describe('Preencher incorretamente a pagina de simulação', () => {
-//     context('como um associado', () => {
+describe('Preencher incorretamente a pagina de simulação', () => {
+    context('como um associado', () => {
+        // primeiro input
+        it('Preenche valor para aplicar com 19,00 e receber um erro', () => {
+            simulationPage.inputQuantiaParaAplicar().type('19,00')
+            simulationPage.inputQuantiaPoupada().click()
+            simulationPage.errorValorAplicarMsg().should('be.visible')
+        });
+        it('Preenche valor para aplicar com -20,00 e verifica se fica negativo', () => {
+            simulationPage.inputQuantiaParaAplicar()
+                .clear()
+                .type('-20,00')
+                .invoke('val').then(text => {
+                    expect(text).to.equal('20,00')
+                })
+            simulationPage.inputQuantiaPoupada().click()
+            simulationPage.errorValorAplicarMsg().should('not.be.visible')
+        });
 
-//         before(() => { simulationPage.acessarSite() });
-//         it('Ao clicar no botão Simular com campos preenchidos com valores abaixo de 20,00', () => {
+        // segundo input
+        it('Preenche valor que você quer poupar com 19,00', () => {
+            simulationPage.inputQuantiaPoupada().type('19,00')
+            simulationPage.inputQuantiaTempo().click()
+            simulationPage.errorValorInvestirMsg().should('be.visible')
+        })
+        it('Preenche valor que você quer poupar com -19,00', () => {
+            simulationPage.inputQuantiaPoupada().clear().type('-19,00')
+            simulationPage.inputQuantiaTempo().click()
+            simulationPage.errorValorInvestirMsg().should('be.visible')
+        })
 
-//         });
-        // it('Ao preencher valor que você quer aplicar com 19,00 e mudar o foco', () => {
+        // terceiro input
+        it('Preenche com 0 campo Por quanto tempo você quer poupar', () => {
+            simulationPage.inputQuantiaTempo().type('0')
+            simulationPage.inputQuantiaPoupada().click()
+            simulationPage.errorTempoMsg().should('be.visible')
+        });
+        it('Deixa o campo Por quanto tempo você quer poupar vazio', () => {
+            simulationPage.inputQuantiaTempo().clear()
+            simulationPage.inputQuantiaPoupada().click()
+            simulationPage.errorTempoMsg().should('be.visible')
+        });
 
-        // })
-//     })
-// });
+        it('Clicar em Simular', () => {
+            simulationPage.btnSimular().click()
+            simulationPage.tabelaComDados().should('not.be.visible')
+        });
+    })
+});
